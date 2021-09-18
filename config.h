51be1a2d0d89243ca34d2bd8f25895827d314611
fanboimsft <fanboimsft@icloud.com>
2021-09-18 16:05:15 +0700

    qemu: default to floating

--- config.h
+++ config.h
@@ -30,9 +30,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	/* class                instance      title       tags mask     isfloating   monitor */
+	{ "Gimp",                 NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",              NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "qemu-system-x86_64",   NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
